30ec51c52c2743376b2a6f86d13833b1104d924d
Raphael <raphaelrdgss.006@gmail.com>
2024-11-21 00:03:33 -0300

    Update to build for itch.io

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,20 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-    pc: `Este é meu computador, é onde produzo meus jogos. Eu programo em JavaScript, TypeScript, Lua, C++, Java, C#, GDScript e BluePrints. Eu utilizo alguma dessas linguagens para bibliotecas disponiveis e
-    também Game Engines, como Unreal Engine, Unity e Godot. Além de programar, eu também estudo programação em geral, mas foco em aplicativos e jogos. No meu computador também faço minhas edições de vídeo
-    no pacote Adobe e estudo Produção Musical`,
-    tv: `Esta é a minha TV. Gosto de vir aqui para ver meus jogos, vou te mostrar os meus projetos postados no Itch.io :
+    pc: `Este eh meu computador, eh onde produzo meus jogos. Eu programo em JavaScript, TypeScript, Lua, C++, Java, C#, GDScript e BluePrints. Eu utilizo alguma dessas linguagens para bibliotecas disponiveis e
+    tambem Game Engines, como Unreal Engine, Unity e Godot. Alem de programar, eu tambem estudo programacao em geral, mas foco em aplicativos e jogos. No meu computador tambem faco minhas edicoes de video
+    no pacote Adobe e estudo Producao Musical`,
+    "cs-degree": `Esse eh meu diploma do Ensino Medio. Fiz ele na ETEC de Lorena no curso de TI, Tecnico de Informatica para Internet. La eu conheci programacao e aprendi os fundamentos basicos de algoritmos e programacao.`,
+  "sofa-table": `Aqui eh meu sofa. Gosto de relaxar aqui e assistir series, filmes e jogar video game.`,
+    tv: `Esta eh a minha TV. Gosto de vir aqui para ver meus jogos, vou te mostrar os meus projetos postados no Itch.io :
      <a href="https://unreal-god-games-makers.itch.io/fps-scifi-prototype" target="_blank">FPS-SciFi Prototype</a>, <a href="https://unreal-god-games-makers.itch.io/the-end-of-drugs-world" target="_blank">The End of Drugs World</a>,
-    <a href="https://unreal-god-games-makers.itch.io/dodge-the-bugs" target="_blank">Dodge The Bugs</a> e por fim o jogo que você está jogando agora <a href="https://www.youtube.com/@MelkeyDev" target="_blank">2D Game Portifolio</a>`,
-    bed: `Esta é minha cama, aqui eu descanso para conseguir ter novas ideias e pensar em novos projetos, além de conseguir absorver todo o conhecimento aprendido no dia`,
-    resume: `Aqui está minha mesa e aqui é meu resumo. <a href="https://github.com/Xalaphas/Xalaphas/blob/main/README.md" target="_blank">Acesse meu GitHub</a>
-    Me chame no email raphaelrdgss.006@gmail.com se tiver interesses em alguma oportunidade de trabalho! E se quiser mais detalhes accesse meu <a href="https://www.github.com/Xalaphas/Xalaphas" target="_blank">currículo!</a>`,
-    projects: `Informação sobre esse projeto : Foi feito com o framework Kaboom.js que utiliza JavaScript e TypeScript para desenvolver jogos.
-    Texto é renderizado com HTML/CSS. Então o texto que está lendo agora não é renderizado com um canvas. Fique atento no meu Itch.io que terá novos jogos em diferentes
+    <a href="https://unreal-god-games-makers.itch.io/dodge-the-bugs" target="_blank">Dodge The Bugs</a> e por fim o jogo que voce esta jogando agora <a href="https://www.youtube.com/@MelkeyDev" target="_blank">2D Game Portifolio</a>`,
+    bed: `Esta eh minha cama, aqui eu descanso para conseguir ter novas ideias e pensar em novos projetos, alem de conseguir absorver todo o conhecimento aprendido no dia`,
+    resume: `Aqui esta minha mesa e aqui eh meu resumo. <a href="https://github.com/Xalaphas/Xalaphas/blob/main/README.md" target="_blank">Acesse meu GitHub</a>
+    Me chame no email raphaelrdgss.006@gmail.com se tiver interesses em alguma oportunidade de trabalho! E se quiser mais detalhes accesse meu <a href="https://www.github.com/Xalaphas/Xalaphas" target="_blank">curriculo!</a>`,
+    projects: `Informacao sobre esse projeto : Foi feito com o framework Kaboom.js que utiliza JavaScript e TypeScript para desenvolver jogos.
+    Texto é renderizado com HTML/CSS. Entao o texto que esta lendo agora nao eh renderizado com um canvas e por isso nao esta aceitando caracteres especiais e acentuacao. Fique atento no meu Itch.io que terá novos jogos em diferentes
     Game Engines e bibliotecas. Acesse aqui a minha conta: <a href="https://unreal-god-games-makers.itch.io" target="_blank">Itch.io</a>`,
-    library: `Estudos são importante para qualquer área, quando deseja adquirir conhecimento. Por isso para aprender programação eu assisto tutoriais no Youtube e realizei cursos na Udemy`,
-    exit: `Se você quiser sair daqui, apenas feche a aba`,
-  };
+    library: `Estudos sao importante para qualquer area, quando deseja adquirir conhecimento. Por isso para aprender programacao eu assisto tutoriais no Youtube e realizei cursos na Udemy`,
+    exit: `Se voce quiser sair daqui, apenas feche a aba`,
+};
